2d6e6159fad310c11d270883521a9fd3ad7c5884
ali <developerali03@gmail.com>
2024-05-29 20:19:26 +0600

    upload all file

---
+++ src/data.js
@@ -0,0 +1,44 @@
+const studentResult = [
+    {
+        name: "Mohammad Ali",
+        age: 37,
+        roll: 33,
+        reg: "1234487",
+        mark: {
+            bangla: 45,
+            english: 89,
+            math: 56,
+            science: 66,
+            social: 77,
+            reli: 55,
+        },
+    },
+    {
+        name: "Johir",
+        age: 24,
+        roll: 35,
+        reg: "445566",
+        mark: {
+            bangla: 49,
+            english: 79,
+            math: 54,
+            science: 61,
+            social: 70,
+            reli: 58,
+        },
+    },
+    {
+        name: "Sumon",
+        age: 23,
+        roll: 39,
+        reg: "123498",
+        mark: {
+            bangla: 57,
+            english: 56,
+            math: 87,
+            science: 60,
+            social: 74,
+            reli: 58,
+        },
+    },
+];
